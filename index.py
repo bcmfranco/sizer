ac3f88df81636bd2fed726cff8f5091db2d1f92f
bcmfranco <bruno@codev.com.ar>
2023-07-16 11:54:32 -0300

    Add get contour behavior

--- index.py
+++ index.py
@@ -5,6 +5,7 @@
 
 import cv2
 import numpy as np
+import matplotlib as plt
 
 print("sizer running")
 
@@ -13,13 +14,30 @@ native_camera = 1
 usb_camera = 0
 
 #Declaro la captura de imagen
-cap = cv2.VideoCapture(usb_camera)
+cap = cv2.VideoCapture(native_camera)
 
 while(True):
-    ret, frame = cap.read()
+    _, frame = cap.read()
+    blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([38, 86, 0])
+    upper_blue = np.array([121, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    contours,hierachy=cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    print(contours)
+
+    for contour in contours:
+        cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
+
     cv2.imshow('frame',frame)
+    cv2.imshow('mask', mask)
+
+    #Cerrar la cámara
     if cv2.waitKey(1) & 0xFF == ord('q'): #La q produce un escape
         break
-
+        
 cap.release()
 cv2.destroyAllWindows()
+
---
+++ utlis.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import matplotlib as plt
+
+# def closeCam():
+#     if cv2.waitKey(1) & 0xFF == ord('q'): #La q produce un escape
+#         break
